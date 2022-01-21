8f210943dc36a0c06d6ed64005a679ff7729f8bd
Raj Joshi <rajjoshi@Rajs-MBP.lan>
2022-01-21 12:19:28 +0800

    added string interpolation

--- my-first-app/src/app/server/server.component.ts
+++ my-first-app/src/app/server/server.component.ts
@@ -3,7 +3,18 @@ import { Component } from "@angular/core";
 @Component({
   selector:'app-server',
   templateUrl:'./server.component.html',
+  styleUrls:['./server.component.css'],
 })
 export class ServerComponent{
+    playerID: number = 11;
+    playerstatus: string = 'online';
+    gameName = 'COD: Modern Warfare';
+    getPlayerID(){
+      return this.playerID;
+    }
+
+    getPlayerStatus(){
+      return this.playerstatus;
+    }
 
 }
