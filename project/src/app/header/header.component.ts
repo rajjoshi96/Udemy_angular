ddeaaf41916c18adadc707b0f1ec5a75d7754ad2
Raj Joshi <rajjoshi@Rajs-MacBook-Pro.local>
2022-01-25 17:11:11 +0800

    added navigation to pages in the header section

--- project/src/app/header/header.component.ts
+++ project/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
 @Component({
   selector:'app-header',
   templateUrl:'./header.component.html'
@@ -6,5 +6,9 @@ import { Component } from "@angular/core";
 })
 
 export class HeaderComponent{
+  @Output() featureSelected = new EventEmitter<string>();
 
+  onSelect(feature:string){
+    this.featureSelected.emit(feature);
+  }
 }
